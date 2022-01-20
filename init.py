73ebf7ee856d7698fc72809548d35568b2d1e94f
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-01-20 14:07:44 -0500

    pylint_test

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
-"""
-A test python function to avoid pylint from complaining everytime a push happens
-"""
+
+"""Init script to check if pylint works """
 def test_func():
+    """A test python function to avoid pylint from complaining everytime a push happens """
     return "Hello group"
 if __name__ == '__main__':
     print(test_func())
