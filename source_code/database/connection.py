ecd7763e0f879f8ca9a5774f93d97fe4c691c525
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-01 01:25:13 -0500

    written functions for db framework

--- source_code/database/connection.py
+++ source_code/database/connection.py
@@ -10,6 +10,7 @@ Import statements
 """
 import pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 
 def get_database():
@@ -17,9 +18,89 @@ def get_database():
     try:
         CONNECTION_STRING = "mongodb+srv://administrator:appsrdsf123@pythonproject.bafek.mongodb.net/rental_system?retryWrites=true&w=majority"
         client = MongoClient(CONNECTION_STRING)
-        return client['rental_system']
+        db =  client['rental_system']
+        return db
     except Exception as e:
+        print("\n An exception occured in connecting to db")
         return e
+
+''' Insert one record into collection 
+Purpose: The below function is used to insert a record into a specific collection
+Params : Collection name & data to be inserted
+'''
+#Parvathy's case: c_name = rentalInfo data: {}
+def insert_into_collection(c_name,data):
+    dataInserted = False
+    try:
+        c_name.insert_one(data)
+        dataInserted = True
+        return dataInserted
+    except Exception as e:
+        print("\n Error in inserting data in collection {}".format(c_name))
+        dataInserted = False
+        return dataInserted
+    
+
+''' Get one record 
+Purpose : The below function is used to retrieve one record from the database
+Params : Collection name & query field
+'''
+
+# Rachel's case : query : {userid:_id usercollection}
+def get_one_record(c_name,query):
+    try:
+       data = c_name.find_one(query)
+       return data
+    except Exception as e:
+        print("\n Could not find the record in collection {}".format(c_name))
+        return None
+
+'''Get many records
+Purpose: The below function is used to get many records from  a specific collection
+Params : Collection name 
+'''
+def get_many_records(c_name)
+    try:
+        data = c_name.find()
+        return data
+    except Exception as e:
+        print("\n Could not find any record in collection {}".format(c_name))
+        return None
+
+'''Update one record
+Purpose: The below function is used to edit a record from a specific collection
+Params : Collection name,data to be inserted,query
+'''
+def update_one_record(c_name,data,query):
+    recordUpdated = False
+    try:
+        current_record = c_name.find_one(query)
+        updated_data = {'$set':data}
+        c_name.update_one(current_record,updated_data)
+        recordUpdated = True
+        return recordUpdated
+    except Exception as e:
+        print("\n Could not update the record in collection {}".format(c_name))
+        recordUpdated = False
+        return recordUpdated
+
+
+'''Delete one record
+Purpose: The below function is used to delete a record from a specific collection
+Params : Collection name and query
+'''
+def delete_one_record(c_name,query):
+    recordDeleted = False
+    try:
+        c_name.delete_one(query)
+        recordDeleted = True
+        return recordDeleted
+    except Exception as e:
+        print("\n Could not delete the record in collection {}".format(c_name))
+        recordDeleted = False
+        return recordDeleted
+
+
 
 if __name__ == "__main__":    
     dbname = get_database()
