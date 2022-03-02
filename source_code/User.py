5ba6ac0a2a16c0312f1119aa5d14ec2107cb9381
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-02 11:44:23 -0500

    Added user stubs

--- source_code/User.py
+++ source_code/User.py
@@ -10,10 +10,36 @@ Import statements
 """
 import database.connection as db
 
-'''Create user record
+'''Signup function
 Purpose: The below function is used to create a user 
 Params : None
 Return value : None
 '''
-def create_user():
+def signup():
+    pass
+
+
+'''Login function
+Purpose: The below function is used to login a user 
+Params : None
+Return value : Boolean
+'''
+def login():
+    pass
     
+
+'''Get one user information
+Purpose: The below function is used to return a  single user data 
+Params : None
+Return value : None
+'''
+def get_one_user():
+    pass
+
+'''Get multiple user information
+Purpose: The below function is used to return multiple user data
+Params : None
+Return value : None
+'''
+def get_multiple_users():
+    pass
