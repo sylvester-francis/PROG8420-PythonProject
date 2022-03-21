81cad5399bd0a2a6f714959b7f5377168499fc38
Rachel Denzil <rachel.jasmine.rjd@gmail.com>
2022-03-21 00:43:00 -0400

    BuildingInfo

--- source_code/Building.py
+++ source_code/Building.py
@@ -2,7 +2,7 @@
 Code created by Rachel Denzil
 Student ID : 8806655
 Created date : 03 March 2022
-Last Modified date : 03 March 2022
+Last Modified date : 15 March 2022
 Last Modified by  : Rachel Denzil
 """
 import database.connection as db
@@ -15,6 +15,11 @@ c_user = client["users"]
 building_list = []
 query = {}
 
+'''Get one building information
+Purpose: The below function is used to return a single building data 
+Params : None
+Return value : None
+'''
 def get_apartment_info():
     try:
         userquery = {}
@@ -31,7 +36,11 @@ def get_apartment_info():
     except Exception as e:
         print("\n Building info not found in collection {}, exception {}".format(c_name,e.__name__))
 
-
+'''Get multiple building information
+Purpose: The below function is used to return multiple building data
+Params : None
+Return value : building_list -> List of all the users present in the collection
+'''
 def get_multiple_buildingInfo():
     global building_list
     buildings = db.get_many_records(c_name)
@@ -39,14 +48,93 @@ def get_multiple_buildingInfo():
         building_list.append(i)
     return building_list
 
-#Delete,update,save - Building info  - #TODO
+'''Create building information
+Purpose: The below function is used to create building information 
+Params : None
+Return value : Boolean
+'''
+def create_buildingInfo():
+    try:
+        data = {
+        "buildingName":"BlueTwo",
+        "Address":"665544 Scott Lane",
+        "City":"Bowmanville",
+        "postalCode":"L1K",
+        "province":"ON",
+        "isFurnished":False,
+        "isParkingAvailable":True,
+        "storageAreaAvailable":True,
+        "petFriendly":True,
+        "noOfWashrooms":"2",
+        "userId": ObjectId("621ef1e586ed827ec8845a12")
+        }
+        data_inserted = db.insert_into_collection(c_name,data)
+        print(data_inserted)
+        return data_inserted
+    except Exception as e:
+        print("\n Error creating new record to rental info collection due to exception {} ".format(e.__name__))
+        return None
+
+'''Update building information
+Purpose: The below function is used to update building data
+Params : None
+Return value : Boolean
+'''
+def update_buildingInfo():
+    try:
+        global query
+        data = {
+        "buildingName":"BlueTwo",
+        "Address":"665544 Scott Lane",
+        "City":"Whitby",  #update city
+        "postalCode":"L1K",
+        "province":"ON",
+        "isFurnished":False,
+        "isParkingAvailable":True,
+        "storageAreaAvailable":True,
+        "petFriendly":True,
+        "noOfWashrooms":"2",
+        "userId": ObjectId("621ef1e586ed827ec8845a12")
+        }
+        query["userId"] = ObjectId("621ef1e586ed827ec8845a12")
+        data_updated = db.update_one_record(c_name,data,query)
+        print(data_updated)
+        query = {}
+        return data_updated
+    except Exception as  e:
+        print("\n Error updating the user due to exception {} ".format(e.__name__))
+        return None
+
+
+'''Delete building information
+Purpose: The below function is used to delete building data
+Params : None
+Return value : Boolean
+'''
+def delete_buildingInfo():
+    try:
+        global query
+        query["userId"] = ObjectId("621ef1e586ed827ec8845a12")
+        data_deleted = db.delete_one_record(c_name,query)
+        print(data_deleted)
+        return data_deleted
+    except Exception as e:
+        print("\n Error deleting the rent info due to exception {} ".format(e.__name__))
+        return None
+
 
 if __name__ == "__main__":  
 
     try:
-        data = get_apartment_info()
-        print(data)
-    # data = get_multiple_buildingInfo()
-    # print(data)
+        # data = get_apartment_info()
+        # print(data)
+        # data = get_multiple_buildingInfo()
+        # print(data)
+        # data = create_buildingInfo()
+        # print(data)
+        # data = update_buildingInfo()
+        # print(data)
+        # data = delete_buildingInfo()
+        # print(data)
     except Exception as e:
         print(e)
