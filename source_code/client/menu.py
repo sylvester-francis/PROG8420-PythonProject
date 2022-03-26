45682a05b1283521fea4efa0d05d4c0fbabc94fa
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-26 00:44:45 -0400

    added menu starter code

---
+++ source_code/client/menu.py
@@ -0,0 +1,22 @@
+"""
+Code created by Sylvester Francis
+Student ID : 8735728
+Created date : 27 February 2022
+Last Modified date : 27 February 2022
+Last Modified by  : Sylvester Francis
+"""
+import sys
+sys.path.append("..")
+import backend.User as user
+import backend.RentalInfo as rental
+import backend.Building as building
+import backend.apartment as apartment
+
+'''Main menu 
+Purpose: The below function is used to display the main menu 
+Params : None
+Return value : None
+'''
+def menu():
+    pass
+menu()
