b9cd6bf5e53d8297e88f70f4c0c00c5e26b5d1a3
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-26 15:33:10 -0400

    Added signup client - skeleton function

--- source_code/client/userhelper.py
+++ source_code/client/userhelper.py
@@ -10,6 +10,7 @@ sys.path.append("..")
 import backend.User as user
 from getpass import getpass
 from helper import clear
+userTypes = {1:'Tenant',2:'Owner',3:'Staff'}
 
 ''' Login helper
 Purpose: The below function is used as a helper function for login 
@@ -24,9 +25,37 @@ def login_helper():
     username = input("\n Enter your username: ")
     password = getpass("\n Enter the password: ")
     print(username,password)
+
 def signup_helper():
     clear()
     print("\n ********************************************************************************** \n")
     print(" \n Signup")
     print("\n ********************************************************************************** \n")
+    firstname = input("\nEnter your First Name: ")
+    lastname  = input("\nEnter your Last Name:  ")
+    email     = input("\n Enter your Email ID:  ")
+    username  = input("\n Enter your preferred username: ")
+    password  = getpass("\n Enter your password: ")
+    confirmpassword = getpass("\n Re-enter the password: ")
+    if password == confirmpassword:
+        print("\n Password matches")
+    else:
+        print("\n Password doesnot match")
+    select_userType = input("\n Please select the type of user: \n1.Tenant \n2.Owner \n3.Staff \n Please type one of the following options(1,2 or 3)")
+    userType =''
+    if select_userType == '1':
+        userType = userTypes[1]
+    elif select_userType == '2':
+        userType= userTypes[2]
+    elif select_userType == '3':
+        userType= userTypes[3]
+    else:
+        print("\n Invalid option")
+    phno = input("\n Enter your phone number: ")
+    data = {}
+    data.update({'FirstName':firstname,'LastName':lastname,'email':email,'username':username,'password':password,'userType':userType,'phoneNumber':phno})
+    print(data)
     
+
+    
+
