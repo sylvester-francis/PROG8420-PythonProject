33cb5531dd9329dd9368f7fb452e7274b20119f4
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-25 23:56:31 -0400

    modifying backend user code to get input from client

--- source_code/backend/User.py
+++ source_code/backend/User.py
@@ -20,29 +20,15 @@ Global Variables
 client = db.get_database()
 c_name = client["users"] # c_name => Collection Name
 user_list = []
-query = {}
 
 '''Signup function
 Purpose: The below function is used to create a user 
 Params : None
 Return value : None
 '''
-
-#An example of create function
-def signup():
-    try: 
-        data = {
-        "FirstName": "Sylvester",
-        "LastName": "Francis",
-        "email": "sylvester@phoca.cz",
-        "username": "sylvester",
-        "password": "ABCDEFG",
-        "userType": "Tenant",
-        "phoneNumber": "548-333-1624",
-        "buildingId": ObjectId("621ef1e586ed827ec8845a12")
-        } 
+def signup(data):
+    try:  
         data_inserted = db.insert_into_collection(c_name,data)
-        print(data_inserted)
         return data_inserted
     except Exception as e:
         print("\n Error signing up to the system due to exception {} ".format(e.__name__))
@@ -53,13 +39,8 @@ Purpose: The below function is used to login a user
 Params : None
 Return value : Boolean
 '''
-def login():
+def login(query):
     try:
-        global query
-        username = input("\n Enter the username:") 
-        password = getpass("\n Enter the password:")
-        query["username"] = username
-        query["password"] = password
         user = db.get_one_record(c_name,query)
         return user
     except Exception as e:
@@ -76,11 +57,8 @@ Purpose: The below function is used to return a  single user data
 Params : None
 Return value : None
 '''
-def get_one_user():
+def get_one_user(query):
     try:
-        global query
-        lname = "Summerlee"
-        query['LastName'] = lname
         user = db.get_one_record(c_name,query)
         query = {}
         return user
@@ -104,23 +82,10 @@ Purpose: The below function is used to update user data
 Params : None
 Return value : Boolean
 '''
-def update_user():
+def update_user(query,data):
     try:
-        global query
-        data = {
-        "FirstName": "Sylvester",
-        "LastName": "Francis",
-        "email": "sylvester@gmail.com",
-        "username": "sylvester",
-        "password": "ABCDEFG",
-        "userType": "Owner",
-        "phoneNumber": "548-333-1624",
-        "buildingId": ObjectId("621ef1e586ed827ec8845a12")
-        }  
-        query["FirstName"] = "Sylvester"
         data_updated = db.update_one_record(c_name,data,query)
         print(data_updated)
-        query = {}
         return data_updated
     except Exception as  e:
         print("\n Error updating the user due to exception {} ".format(e.__name__))
@@ -131,10 +96,8 @@ Purpose: The below function is used to update user data
 Params : None
 Return value : Boolean
 '''
-def delete_user():
+def delete_user(query):
     try:
-        global query
-        query["FirstName"] = "Sylvester"
         data_deleted = db.delete_one_record(c_name,query)
         print(data_deleted)
         return data_deleted
@@ -142,21 +105,3 @@ def delete_user():
         print("\n Error deleting the user due to exception {} ".format(e.__name__))
         return None
 
-
-if __name__ == "__main__":  
-    try:
-        data = get_one_user()
-        print(data)
-        # data = login()
-        # if data != None:
-            # print(data)
-        # else:
-            # print(" \n No record found")
-        # data = signup()
-        # print(data)
-        # data = update_user()
-        # print(data)
-        # data = delete_user()
-        # print(data)
-    except Exception as e:
-        print(e.__name__)
