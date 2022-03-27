4fd73aeb0705805194cb63ed2301e465ab1528a9
sylvester-francis <sylvesterranjithfrancis@gmail.com>
2022-03-26 22:54:50 -0400

    Started with login path

--- source_code/client/userhelper.py
+++ source_code/client/userhelper.py
@@ -29,6 +29,13 @@ def login_helper():
     password = getpass("\n Enter the password: ")
     print(username,password)
 
+
+
+''' Signup helper
+Purpose: The below function is used as a helper function for signup
+Params : None
+Return value : issignedup -> Boolean 
+'''
 def signup_helper():
     clear()
     data = {}
@@ -51,7 +58,7 @@ def signup_helper():
     if select_userType == '1':
         userType = userTypes[1]
         data.update({'FirstName':firstname,'LastName':lastname,'email':email,'username':username,'password':password,'userType':userType,'phoneNumber':phno})
-        issignedUp = tenant_path(data)
+        issignedUp = tenant_path(data,'signup')
     elif select_userType == '2':
         userType= userTypes[2]
         owner_path()
@@ -61,77 +68,98 @@ def signup_helper():
     else:
         print("\n Invalid option")
     return issignedUp
-    
-
-def tenant_path(data):
-    signedup = False
-    errorApartment = True
-    errorBuilding = True
-    clear()
-    print("\nHere are the list of buildings available: ") 
-    buildings = building.get_multiple_buildingInfo()
-    building_list = {}
-    print("\n ********************************************************************************** \n")
-    for index,item in enumerate(buildings):
-        print("\n ****************************** General-Details of Building - {0} ********************************************* \n".format(index+1))
-        print("\n Name of the building : {0} \n Address: {1} \n City: {2} \n PostalCode: {3} \n Province: {4} ".format(item['buildingName'],item['Address'],item['City'],item['postalCode'],item['province']))
-        print("\n ****************************** Facilities of Building - {0} ********************************************* \n".format(index+1))
-        print("\n Furnished :{0}, \n Parking : {1}, \n Pet-Friendly: {2}, \n Storage: {3}".format(item['isFurnished'],item['isParkingAvailable'],item['petFriendly'],item['storageAreaAvailable']))
-        building_list[index+1] = item['_id']
-    print("\n ********************************************************************************** \n")
-    while errorBuilding:
-        try:
-            select_building = int(input("\n Enter the building that you are interested in: "))
-            if select_building not in building_list.keys():
-                raise KeyError("Building not found please try again")
-            elif select_building in building_list.keys():
-                q = {}
-                q['buildingId'] = ObjectId(building_list[select_building])
-                q['isAvailable'] = True
-                apartmentlist = apartment.get_multiple_apartmentInfo(q)
-                if len(apartmentlist) > 0:
-                    errorBuilding = False
-                else:
-                    raise KeyError("Sorry,No apartments available in this building,try again")
-        except KeyError as ke:
-            print("\n{0}".format(ke))
-            continue  
-        errorBuilding = False      
-    apartmentid = {}
-    clear()
-    print("\n ********************************************************************************** \n")
-    for index,item in enumerate(apartmentlist):
-        print("\n ****************************** Facilities of Apartment - {0} ********************************************* \n".format(index+1))
-        print("\n Furnished :{0}, \n No Of Washrooms : {1}, \n Ensuite Washroom: {2}, \n Ensuite Laundry: {3}, \n Rent Price: {4}".format(item['isFurnished'],item['noOfWashrooms'],item['hasEnsuiteWashroom'],item['hasEnsuiteLaundry'],0))
-        apartmentid[index+1] = item['_id']
-    print("\n ********************************************************************************** \n")
-    print("\n ********************************************************************************** \n")
-    while errorApartment:
-        try:
-            select_apartment = int(input("\n Enter the apartment that you are interested in: "))
-            if select_apartment not in apartmentid.keys():
-                raise KeyError("Apartment not found please try again")
-        except KeyError as e:
-            print("\n {0}".format(e))
-            continue
-        errorApartment = False
-    data['buildingId'] = building_list[select_building]
-    data['apartmentId'] = apartmentid[select_apartment]
-    user_obj = user.signup(data)
-    if user_obj != None:
-        print("\n User signed up successfully")
-        signedup = True
-        return signedup
-    else:
-        print("\n Error in signing up User, please try again")
-        return signedup
-    
 
 
-def owner_path():
-    pass
-def staff_path():
-    pass
+
+''' Tenant path
+Purpose: The below function is used as a added function  for tenant signup 
+Params : data -> Dictionary with all user inputs, typeAction -> [login,signup]
+Return value : signedup -> Boolean 
+'''  
+
+def tenant_path(data,typeAction):
+    if typeAction == 'signup':
+        signedup = False
+        errorApartment = True
+        errorBuilding = True
+        clear()
+        print("\nHere are the list of buildings available: ") 
+        buildings = building.get_multiple_buildingInfo()
+        building_list = {}
+        print("\n ********************************************************************************** \n")
+        for index,item in enumerate(buildings):
+            print("\n ****************************** General-Details of Building - {0} ********************************************* \n".format(index+1))
+            print("\n Name of the building : {0} \n Address: {1} \n City: {2} \n PostalCode: {3} \n Province: {4} ".format(item['buildingName'],item['Address'],item['City'],item['postalCode'],item['province']))
+            print("\n ****************************** Facilities of Building - {0} ********************************************* \n".format(index+1))
+            print("\n Furnished :{0}, \n Parking : {1}, \n Pet-Friendly: {2}, \n Storage: {3}".format(item['isFurnished'],item['isParkingAvailable'],item['petFriendly'],item['storageAreaAvailable']))
+            building_list[index+1] = item['_id']
+        print("\n ********************************************************************************** \n")
+        while errorBuilding:
+            try:
+                select_building = int(input("\n Enter the building that you are interested in: "))
+                if select_building not in building_list.keys():
+                    raise KeyError("Building not found please try again")
+                elif select_building in building_list.keys():
+                    q = {}
+                    q['buildingId'] = ObjectId(building_list[select_building])
+                    q['isAvailable'] = True
+                    apartmentlist = apartment.get_multiple_apartmentInfo(q)
+                    if len(apartmentlist) > 0:
+                        errorBuilding = False
+                    else:
+                        raise KeyError("Sorry,No apartments available in this building,try again")
+            except KeyError as ke:
+                print("\n{0}".format(ke))
+                continue  
+            errorBuilding = False      
+        apartmentid = {}
+        clear()
+        print("\n ********************************************************************************** \n")
+        for index,item in enumerate(apartmentlist):
+            print("\n ****************************** Facilities of Apartment - {0} ********************************************* \n".format(index+1))
+            print("\n Furnished :{0}, \n No Of Washrooms : {1}, \n Ensuite Washroom: {2}, \n Ensuite Laundry: {3}, \n Rent Price: {4}".format(item['isFurnished'],item['noOfWashrooms'],item['hasEnsuiteWashroom'],item['hasEnsuiteLaundry'],0))
+            apartmentid[index+1] = item['_id']
+        print("\n ********************************************************************************** \n")
+        print("\n ********************************************************************************** \n")
+        while errorApartment:
+            try:
+                select_apartment = int(input("\n Enter the apartment that you are interested in: "))
+                if select_apartment not in apartmentid.keys():
+                    raise KeyError("Apartment not found please try again")
+            except KeyError as e:
+                print("\n {0}".format(e))
+                continue
+            errorApartment = False
+        data['buildingId'] = building_list[select_building]
+        data['apartmentId'] = apartmentid[select_apartment]
+        user_obj = user.signup(data)
+        if user_obj != None:
+            print("\n User signed up successfully")
+            signedup = True
+            return signedup
+        else:
+            print("\n Error in signing up User, please try again")
+            return signedup
+    elif typeAction == 'login':
+        print("\n Tenant login path")
+
+
+
+''' Owner path
+Purpose: The below function is used as a added function  for owner signup path/login path
+Params : data -> Dictionary with all user inputs,typeAction -> [login,signup]
+Return value : signedup -> Boolean 
+'''  
+def owner_path(data,typeAction):
+    print("\n Owner path called")
+
+''' Staff path
+Purpose: The below function is used as a added function  for staff signup 
+Params : data -> Dictionary with all user inputs,typeAction -> [login,signup]
+Return value : signedup -> Boolean 
+'''  
+def staff_path(data,typeAction):
+    print("\n Staff path called")
     
 
     
